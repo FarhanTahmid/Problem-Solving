6b4786df9b5be9e0d76e38cfad1d3fe417f2e586
Farhan Tahmid <farhantahmid881@gmail.com>
2022-09-10 20:50:53 +0600

    problem3

--- Array Problems - Geeks For Geeks/Level 1/problem3.cpp
+++ Array Problems - Geeks For Geeks/Level 1/problem3.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
+
+
+//unsolved
+
 using namespace std;
 int main(){
     int t;
     cin>>t;
-    while(t){
-        int n;
+    while(t>0){
+        int n,temp;
         cin>>n;
         int arr[n];
+        int arr2[n]={0};
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
-        int arr2[n];
-        for(int i=n;i>0;i--){
-            arr2[i]=arr[i];
+        for(int i=0,j=n-1;i<n/2;i++,j--){
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
         }
         for(int i=0;i<n;i++){
-            cout<<arr2[i]<<" ";
+            cout<<arr[i]<<" ";
         }
+        cout<<endl;
         t--;
     }
 }
